17b7350caf130dd2bc2072455c434ea857f8e483
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-20 23:28:17 +0200

    ..

---
+++ seed_data.py
@@ -0,0 +1,16 @@
+import psycopg2
+conn = psycopg2.connect('postgresql://quotes_db_zb2f_user:tFp9kNbdMeQ62DK95FIQi8LSsYwABLmH@dpg-d8p8hmpo3t8c73ef1cmg-a.frankfurt-postgres.render.com/quotes_db_zb2f')
+cursor = conn.cursor()
+
+cursor.execute('''
+    INSERT INTO customers (Customer_Id, name, email, phone, can_email)
+    VALUES
+    (%s, %s, %s, %s, %s),
+    (%s, %s, %s, %s, %s),
+    ON CONFLICT (Customer_id) DO NOTHING
+    ''', (
+        'CUST-01', 'Rorisang Madikgetla', 'rorisangmadikgetla767@gmail.com', '0721426286', True,
+        'CUST-08', 'John Smith', 'madixxwtf@gmail.com', '0662303453', True
+        
+))
+conn.commit()
