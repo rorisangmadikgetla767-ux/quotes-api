017653b3a0f79960e97c84300a015a7f446c9b82
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-20 17:46:43 +0200

    I added model.py comments for non-technical people

--- models.py
+++ models.py
@@ -14,3 +14,6 @@ class LineItem(BaseModel):
     quantity :int
     unit_price: float
     
+# BaseModel defines the exact shape of data going into or out of an endpoint - It validates incoming JSON, Rejects requests that do not match. It also auto-generates the request/response schema shown in Swagger.  
+# Every field without `Optional[...] = None ` is required and FASTAPI returns a 422 error if it is missing from the request body.
+# Optional[str] = None -> means the field can be left out of the JSON, or explicitly set to null
