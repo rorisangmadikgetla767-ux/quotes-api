c264f89c8b4dd944c62f2674727b377e14a5aa93
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-21 12:23:45 +0200

    I created a new file called email utils.py

---
+++ emai;_utils.py
@@ -0,0 +1,10 @@
+def send_email(to: str, subject: str, body: str):
+    """ 
+    Stub email sender 
+    It prints instead of actually sending
+    """
+    print(f"---EMAIL STUB---")
+    print(f"To: {to}")
+    print(f"Subject: {subject}")
+    print(f"Body:\n{body}")
+    print(f"---END EMAIL---")
