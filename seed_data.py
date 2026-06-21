cbd8a27cc963aba374999f0ff6b6ad3ebe008f6c
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-21 11:46:58 +0200

    ..

--- seed_data.py
+++ seed_data.py
@@ -29,7 +29,7 @@ cursor.execute('''
     'Q003', 'QN-003', 'CUST-04', 'BENZ003', 'Brake problems', 'Rorisang'
 ))
 conn.commit()
-print('Quote has been seeded successfully..')Q
+print('Quote has been seeded successfully..')
 
 cursor.close()
 conn.close()
