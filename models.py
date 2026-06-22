717f1f65d7968297228295ead4a6dca8e7495853
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-22 15:39:21 +0200

    My first attempt in trying to translate the content in Neil's frontend so my DB can understand

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional,List
+from datetime import date
 
 class Quote(BaseModel):
     Quote_id: str
@@ -17,3 +18,10 @@ class LineItem(BaseModel):
 # BaseModel defines the exact shape of data going into or out of an endpoint - It validates incoming JSON, Rejects requests that do not match. It also auto-generates the request/response schema shown in Swagger.  
 # Every field without `Optional[...] = None ` is required and FASTAPI returns a 422 error if it is missing from the request body.
 # Optional[str] = None -> means the field can be left out of the JSON, or explicitly set to null
+
+class QuoteCreate(Quote):
+    expiry_date: Optional[date] = None
+    notes: Optional[str] = None
+    discount_value: Optional[float] = 0
+    tax_percent: Optional[float] = 15
+    line_items: List[LineItem] = []
