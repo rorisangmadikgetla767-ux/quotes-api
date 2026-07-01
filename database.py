f8cd754ca9abc0f858b9e75118e7d5e10a800ebb
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-07-01 14:27:22 +0200

    I fixed a Typo

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 
 def get_connection():
-    database_url = os.getenv("DATABSE_URL")
+    database_url = os.getenv("DATABASE_URL")
     conn = psycopg2.connect(database_url)
+    print(database_url)
     
     return conn
